1dfbe8acf8d78551febfa751c259b0cdd9c8cc29
Dũng Nguyễn Tấn <dungnguyentan@Dungs-Laptop.local>
2024-05-03 20:40:22 +0800

    chore: update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { Button, Space, Stack, Text } from '@mantine/core';
+import { useRouter } from 'next/router';
 
 export default function HomePage() {
+  const router = useRouter();
   return (
     <>
-      <Stack h="100%" justify="center" align="center">
+      <Stack h="90vh" justify="center" align="center">
         <Text fw={700} h={1}>ONE HEALTH</Text>
         <Space h="md" />
         <Text fs="italic" ta="center">“better experience,
@@ -11,11 +13,11 @@ export default function HomePage() {
         <Space h="md" />
         <Text ta="center">a health and wellness platform provides curated experience for people </Text>
         <Space h="md" />
-        <Button w="95" display="inline-block" bg="dark">
+        <Button w="95" display="inline-block" bg="dark" onClick={() => router.push('/reservation')}>
           Sign Up
         </Button>
         <Space h="md" />
-        <Button w="95" display="inline-block" bg="dark">
+        <Button w="95" display="inline-block" bg="dark" onClick={() => router.push('/reservation')}>
           Log in
         </Button>
       </Stack>
